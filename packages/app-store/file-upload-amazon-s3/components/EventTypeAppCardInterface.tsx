5e63ffaef2972db1d2f3c541d61ff8f718e2bd5e
Rob Jackson <rob@rjackson.me>
2023-05-13 21:36:28 +0100

    Settings for Amazon S3 app

--- packages/app-store/file-upload-amazon-s3/components/EventTypeAppCardInterface.tsx
+++ packages/app-store/file-upload-amazon-s3/components/EventTypeAppCardInterface.tsx
@@ -3,38 +3,50 @@ import { useState } from "react";
 import { useAppContextWithSchema } from "@calcom/app-store/EventTypeAppContext";
 import AppCard from "@calcom/app-store/_components/AppCard";
 import type { EventTypeAppCardComponent } from "@calcom/app-store/types";
-import { Sunrise, Sunset } from "@calcom/ui/components/icon";
+import { useLocale } from "@calcom/lib/hooks/useLocale";
+import { PasswordField, SettingsToggle, TextField } from "@calcom/ui";
 
 import type { appDataSchema } from "../zod";
 
 const EventTypeAppCard: EventTypeAppCardComponent = function EventTypeAppCard({ eventType, app }) {
+  const { t } = useLocale();
   const [getAppData, setAppData] = useAppContextWithSchema<typeof appDataSchema>();
-  const isSunrise = getAppData("isSunrise");
   const [enabled, setEnabled] = useState(getAppData("enabled"));
 
+  const endpoint = getAppData("endpoint");
+  const [customEndpointToggle, setCustomEndpointToggle] = useState(!!endpoint);
+
   return (
-    <AppCard
-      setAppData={setAppData}
-      app={app}
-      switchOnClick={(e) => {
-        if (!e) {
-          setEnabled(false);
-          setAppData("isSunrise", false);
-        } else {
-          setEnabled(true);
-          setAppData("isSunrise", true);
-        }
-      }}
-      switchChecked={enabled}>
-      <div className="mt-2 text-sm">
-        <div className="flex">
-          <span className="ltr:mr-2 rtl:ml-2">{isSunrise ? <Sunrise /> : <Sunset />}</span>I am an AppCard for
-          Event with Title: {eventType.title}
-        </div>{" "}
-        <div className="mt-2">
-          Edit <span className="italic">packages/app-store/{app.slug}/EventTypeAppCardInterface.tsx</span> to
-          play with me
-        </div>
+    <AppCard setAppData={setAppData} app={app} switchOnClick={(e) => setEnabled(e)} switchChecked={enabled}>
+      <div className="space-y-4">
+        <PasswordField
+          name={t("file-upload-amazon-s3_api-key")}
+          value={getAppData("apiKey")}
+          onChange={(e) => setAppData("apiKey", e.target.value)}
+        />
+        <TextField
+          name={t("file-upload-amazon-s3_bucket")}
+          value={getAppData("bucket")}
+          onChange={(e) => setAppData("bucket", e.target.value)}
+        />{" "}
+        <hr className="border-subtle" />
+        <SettingsToggle
+          title={t("file-upload-amazon-s3_endpoint_toggle")}
+          description={t("file-upload-amazon-s3_endpoint_toggle_description")}
+          checked={customEndpointToggle}
+          onCheckedChange={(active) => {
+            setCustomEndpointToggle(active);
+            if (!active) {
+              setAppData("endpoint", undefined);
+            }
+          }}>
+          <TextField
+            name={t("file-upload-amazon-s3_endpoint")}
+            hint={t("file-upload-amazon-s3_endpoint_description")}
+            value={endpoint}
+            onChange={(e) => setAppData("endpoint", e.target.value)}
+          />{" "}
+        </SettingsToggle>
       </div>
     </AppCard>
   );
--- packages/app-store/file-upload-amazon-s3/zod.ts
+++ packages/app-store/file-upload-amazon-s3/zod.ts
@@ -4,7 +4,11 @@ import { eventTypeAppCardZod } from "@calcom/app-store/eventTypeAppCardZod";
 
 export const appDataSchema = eventTypeAppCardZod.merge(
   z.object({
-    isSunrise: z.boolean(),
+    // TODO: Add capability to encrypt and exclude keys from frontend (at the moment, all app data is exposed to frontend)
+    // Maybe appKeys, but they're shared globally instead of per eventType?
+    apiKey: z.string(),
+    bucket: z.string(),
+    endpoint: z.string().optional(),
   })
 );
 export const appKeysSchema = z.object({});
